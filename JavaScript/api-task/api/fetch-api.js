8c6fd1e5bfb6b37e825ea1b11466bd7dc661fe5b
Arulmozhi <arulmozhi.karunagaran@trustrace.com>
2022-12-14 10:50:02 +0530

    File deleted

--- JavaScript/api-task/api/fetch-api.js
+++
@@ -1,7 +0,0 @@
-const apiFile = 'https://jsonplaceholder.typicode.com/todos/';
-getApi();
-async function getApi() {
-    const res = await fetch(apiFile);
-    const myData = await res.text();
-    sessionStorage.setItem('value',myData);
-}
