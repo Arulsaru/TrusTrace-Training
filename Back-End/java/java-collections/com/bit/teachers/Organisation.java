46f3b509a94727b192c2861c269bd9d7b2414476
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-01-04 23:43:01 +0530

    Stack practiced

---
+++ Back-End/java/java-collections/com/bit/teachers/Organisation.java
@@ -0,0 +1,32 @@
+package com.bit.teachers;
+import java.util.*;
+
+public class Organisation {
+    Scanner sc = new Scanner(System.in);
+    Stack <String> teachersStk = new Stack<>();
+    Iterator stackIterator = teachersStk.iterator();
+    private String name;
+    boolean isStackEmpty() {
+        return teachersStk.isEmpty();
+    }
+    String getTeachers() {
+        return sc.nextLine();
+    }
+    void moveTeachersInsideStack() {
+//        name = "ads";
+        name = getTeachers();
+        teachersStk.push(name);
+    }
+
+    void moveTeachersInsideQueue() {
+        name = getTeachers();
+        teachersStk.push(name);
+    }
+    void popATeacher() {
+        teachersStk.pop(); // Empty stack exception
+    }
+    void searchATeacher() {
+        int idx = teachersStk.search("ads");
+        System.out.println(teachersStk.indexOf(idx));
+    }
+}
--- Back-End/java/java-collections/com/bit/teachers/teachers.java
+++ Back-End/java/java-collections/com/bit/teachers/teachers.java
@@ -1,16 +1,16 @@
-package com.bit.teachers;
+package com.bit.Teachers;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class teachers {
+public class Teachers {
     public static void main(String[] args) {
         int userId = 0;
         List<String> teachersNames = Arrays.asList("Arulmozhi", "Hari", "Keerti", "Gowtham");
 //        teachersNames.forEach(name -> System.out.println(name));
 
-        Stream<String> data = teachersNames.stream();
+        Stream<String> data = teachersNames.stream(); // can be used only once...
 //        data.forEach(name -> System.out.println(name));
 //        System.out.println(data.count());
 //        Stream<String> sortedData = data.sorted();
@@ -19,7 +19,10 @@ public class teachers {
 //        Stream<String> mappedData = sortedData.map(name -> name.toLowerCase()) ;
 //        mappedData.forEach(name -> System.out.println(name));
 
-        teachersNames.stream().filter(name -> name.length() % 2 == 0).sorted().map(name -> name.toUpperCase()).forEach(name -> System.out.println(name));
+//        teachersNames.stream().filter(name -> name.length() % 2 == 0).sorted().map(name -> name.toUpperCase()).forEach(name -> System.out.println(name));
 
+        Organisation org = new Organisation();
+        org.moveTeachersInsideStack();
+        org.searchATeacher();
     }
 }
