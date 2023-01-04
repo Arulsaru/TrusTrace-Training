3a1b57fdaf616c612073fd3501f68931706c6b81
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-01-04 23:47:00 +0530

    Queue implemented

---
+++ Back-End/java/java-collections/com/bit/teachers/queueImpl.java
@@ -0,0 +1,24 @@
+package com.bit.teachers;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class queueImpl implements methods {
+    Queue<String> teachersQueue = new PriorityQueue<>();
+    boolean isQueueEmpty() {
+        return teachersQueue.isEmpty();
+    }
+    void printTeachersFromQueue() {
+        if(isQueueEmpty()) {
+            printEmptyMessage("queue");
+        }
+        for(String str: teachersQueue) {
+            System.out.println(str);
+        }
+    }
+
+    @Override
+    public void printEmptyMessage(String location) {
+        System.out.println(location + " is empty");
+    }
+}
