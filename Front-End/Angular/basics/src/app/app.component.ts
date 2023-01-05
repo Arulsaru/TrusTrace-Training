127626cd791562b55933e13ff99685baf7731cac
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-01-05 23:21:57 +0530

    Class, Style, Event, Property, Interpolation  binding practiced

--- Front-End/Angular/basics/src/app/app.component.ts
+++ Front-End/Angular/basics/src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // selector: '.app-root', // class
+  // selector: '[app-root]', // attribute
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basics';
+  name: string = 'Arulmozhi Karunagaran';
+  buttonId: string = 'button-one';
+  isDisabled: boolean = false;
+  colorStyleText: string = 'green-color';
+  color: string = 'aqua';
+  showText: string = '';
+
+  onclick(): void {
+    this.showText = 'Clicked button';
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
 }
