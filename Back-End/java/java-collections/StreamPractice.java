adaaeb6e22ad114af396d9ec5cc975a71cb1387c
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-01-05 11:23:11 +0530

    Stream Conecpt Practiced

---
+++ Back-End/java/java-collections/StreamPractice.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamPractice {
+    public static void main(String[] args) {
+        List<String> teachersNames = Arrays.asList("Arulmozhi", "Hari", "Keerti", "Gowtham");
+//        teachersNames.forEach(name -> System.out.println(name));
+        Stream<String> data = teachersNames.stream(); // can be used only once...
+//        data.forEach(name -> System.out.println(name));
+//        System.out.println(data.count());
+//        Stream<String> sortedData = data.sorted();
+//        sortedData.forEach(name -> System.out.println(name));
+
+//        Stream<String> mappedData = sortedData.map(name -> name.toLowerCase()) ;
+//        mappedData.forEach(name -> System.out.println(name));
+
+//        teachersNames.stream().filter(name -> name.length() % 2 == 0).sorted().map(name -> name.toUpperCase()).forEach(name -> System.out.println(name));
+    }
+}
