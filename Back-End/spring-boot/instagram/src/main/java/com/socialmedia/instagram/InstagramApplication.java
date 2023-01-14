9b6382f99a4885d6c7047de9c02162d2d1030a1e
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-01-14 22:55:45 +0530

    Renamed file

--- Back-End/spring-boot/instagram/src/main/java/com/socialmedia/instagram/InstagramApplication.java
+++ Back-End/spring-boot/instagram/src/main/java/com/socialmedia/instagram/InstagramApplication.java
@@ -1,13 +1,11 @@
-package com.sociaMedia.instagram;
+package com.sociamedia.instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InstagramApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InstagramApplication.class, args);
 	}
-
 }
--- Back-End/spring-boot/instagram/src/test/java/com/socialmedia/instagram/InstagramApplicationTests.java
+++ Back-End/spring-boot/instagram/src/test/java/com/socialmedia/instagram/InstagramApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sociaMedia.instagram;
+package com.sociamedia.instagram;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
