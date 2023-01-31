e526818cfd78248127c3dea77f3b9320c69fc024
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-01-31 18:18:04 +0530

    API Response and Global exception handler updated

---
+++ Back-End/spring-boot/instagram/src/main/java/com/socialmedia/instagram/common/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.socialmedia.instagram.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity handleException(Exception e) {
+        APIResponse apiResponse = new APIResponse();
+        apiResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        apiResponse.setError(e.getMessage());
+        return ResponseEntity.status(500).body(apiResponse);
+    }
+}
