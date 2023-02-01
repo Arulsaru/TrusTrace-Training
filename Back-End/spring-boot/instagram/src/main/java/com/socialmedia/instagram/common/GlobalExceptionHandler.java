d3c378e558e03babf8dc450bdf1fc27bd2ef9e52
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-01 11:58:29 +0530

    Global exception handler updated

--- Back-End/spring-boot/instagram/src/main/java/com/socialmedia/instagram/common/GlobalExceptionHandler.java
+++ Back-End/spring-boot/instagram/src/main/java/com/socialmedia/instagram/common/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity handleException(Exception e) {
+    public ResponseEntity<APIResponse> handleException(Exception e) {
         APIResponse apiResponse = new APIResponse();
         apiResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         apiResponse.setError(e.getMessage());
