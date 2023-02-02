311a2f7eee69375372c9f6237e666edbb334791e
Arulmozhi-Karunagaran <saruarul154@gmail.com>
2023-02-02 12:38:59 +0530

    Test file modified

--- Back-End/spring-boot/instagram/src/test/java/com/socialmedia/instagram/InstagramApplicationTests.java
+++ Back-End/spring-boot/instagram/src/test/java/com/socialmedia/instagram/InstagramApplicationTests.java
@@ -1,11 +1,8 @@
 package com.socialmedia.instagram;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(MockitoJUnitRunner.class)
 class InstagramApplicationTests {
 	@Test
 	void contextLoads() {
